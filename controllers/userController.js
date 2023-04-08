4d27446cfec8ca3fcebc799383b66792e060def9
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-08 18:01:56 -0400

    update userController: add updated user

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,3 +42,17 @@ const userTotal = async () =>
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
     },
+
+    updateUser(req, res){
+        User.findOneAndUpdate({ _id: req.params.userId},
+          { $set: req.body })
+        .then((user) => 
+        !user
+          ? res.status(404).json({ message: 'No user with that Id' })
+          : res.status(200).json('User has been updated')
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    },

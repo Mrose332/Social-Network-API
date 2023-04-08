1544b36de6603d4f1f8c75c23e26ae00c546adcf
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-08 16:55:58 -0400

    update thoughtContoller: added syntax for single thought/create thought

--- controllers/thoughtContoller.js
+++ controllers/thoughtContoller.js
@@ -2,7 +2,7 @@ const { User, Thought } = require("../models");
 
 
 const thoughtController = {
-   
+   //get all thoughts
     getThoughts(req, res){
         Thought.find()
         .then((thoughts) => res.json(thoughts))
@@ -12,3 +12,23 @@ const thoughtController = {
     });
 },
 }
+
+//get single thought
+getSingleThought(req, res)
+    Thought.find({_id: req.params.thoughtId})
+    .select("-__v")
+    .then(async (thought) =>
+    !thought
+      ? res.status(404).json({ message: 'No thought with that ID' })
+      : res.json(thought)
+  )
+  .catch((err) => {
+    console.log(err);
+    return res.status(500).json(err);
+  });
+
+
+createThought(req, res)
+    Thought.create(req.body)
+    .then((thought) => res.json(thought))
+    .catch((err) => res.status(500).json(err));

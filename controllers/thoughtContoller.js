69b1358d95a24a73cd2e1b37ecd87e7970f438ae
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-08 17:44:01 -0400

    updated thoughtController:  added syntax to reaction

--- controllers/thoughtContoller.js
+++ controllers/thoughtContoller.js
@@ -1,40 +1,42 @@
 const { User, Thought } = require("../models");
 
 
+
 const thoughtController = {
-   //get all thoughts
+   // get all thoughts
     getThoughts(req, res){
         Thought.find()
         .then((thoughts) => res.json(thoughts))
         .catch((err) => {
             console.log(err);
             return res.status(500).json(err);
-    });
-},
-}
+        });
+    },
 
-//get single thought
-getSingleThought(req, res)
-    Thought.find({_id: req.params.thoughtId})
-    .select("-__v")
-    .then(async (thought) =>
-    !thought
-      ? res.status(404).json({ message: 'No thought with that ID' })
-      : res.json(thought)
-  )
-  .catch((err) => {
-    console.log(err);
-    return res.status(500).json(err);
-  });
+// get single thought
+    getSingleThought(req, res){
+        Thought.find({_id: req.params.thoughtId})
+        .select("-__v")
+        .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+    },
 
-//create thought
-createThought(req, res)
-    Thought.create(req.body)
-    .then((thought) => res.json(thought))
-    .catch((err) => res.status(500).json(err));
+    //create thought
+    createThought(req, res){
+        Thought.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
 
- //update thought   
-updateThought(req, res)
+//update thought
+    updateThought(req, res){
         Thought.findOneAndUpdate({ _id: req.params.thoughtId},
           { $set: req.body })
         .then((thought) => 
@@ -46,9 +48,10 @@ updateThought(req, res)
         console.log(err);
         res.status(500).json(err);
       });
-    
-      //delete thought
-      deleteThought(req, res)
+    },
+
+    //delete thought
+    deleteThought(req, res){
         Thought.findOneAndDelete({ _id: req.params.thoughtId})
         .then((thought) => 
         !thought
@@ -59,28 +62,12 @@ updateThought(req, res)
         console.log(err);
         res.status(500).json(err);
       });
-    
-// add reaction
-      addReaction(req, res)
-        Thought.findOneAndUpdate(
-          { _id: req.params.thoughtId },
-          { $addToSet: { reactions: req.body } },
-          { runValidators: true, new: true }
-          )
-          .then((thought) =>
-              !thought
-              ? res
-                  .status(404)
-                  .json({ message: 'No thought found with that ID :(' })
-              : res.json(thought)
-          )
-          .catch((err) => res.status(500).json(err));
-      
-  //delete reaction
-      deleteReaction(req, res)
-        Thought.findOneAndUpdate(
+    },
+//add reaction
+    addReaction(req, res){
+      Thought.findOneAndUpdate(
         { _id: req.params.thoughtId },
-        { $pull: { reactions: { reactionId: req.body.reactionId } } },
+        { $addToSet: { reactions: req.body } },
         { runValidators: true, new: true }
         )
         .then((thought) =>
@@ -88,12 +75,29 @@ updateThought(req, res)
             ? res
                 .status(404)
                 .json({ message: 'No thought found with that ID :(' })
-            : res.json({ message: 'DELETED THE REACTION' })
+            : res.json(thought)
         )
         .catch((err) => res.status(500).json(err));
-      
-  
-  
-  
-  module.exports = thoughtController;
-  
+    },
+
+    //delete reaction
+    deleteReaction(req, res){
+      Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.body.reactionId } } },
+      { runValidators: true, new: true }
+      )
+      .then((thought) =>
+          !thought
+          ? res
+              .status(404)
+              .json({ message: 'No thought found with that ID :(' })
+          : res.json({ message: 'DELETED THE REACTION' })
+      )
+      .catch((err) => res.status(500).json(err));
+    },
+}
+
+
+
+module.exports = thoughtController;

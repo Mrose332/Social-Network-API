5925d56a132e7544b267101fd0fdf34640c6b91a
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-08 18:39:07 -0400

    commit changes created thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require("../../controllers/thoughtController");
+
+
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);;
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+router.route('/:thoughtId/reactions/').post(addReaction);
+
+
+module.exports = router;

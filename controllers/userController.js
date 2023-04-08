e9c19f9bd3873a256ffc0d4063253be9687d4c4e
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-04-08 17:58:34 -0400

    update userController: add single user syntax

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,17 @@ const userTotal = async () =>
             return res.status(500).json(err);
         });
     },
+// get one user
+    getSingleUser(req, res){
+        User.find({_id: req.params.userId})
+        .select("-__v")
+        .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+    },
